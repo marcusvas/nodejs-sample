5158f20b349c2f5531a6a5c4509df5523013b550
Danilo Barsotti <mcbarsotti@gmail.com>
2014-07-18 20:14:24 +0000

    identation fixed

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -13,18 +13,17 @@ module.exports = function(app) {
 	});
 	
 	app.get('/register', function(req, res) {
-      res.render('register');
+      	res.render('register');
   	});
   	
 	app.post('/register', function(req, res) {
 		Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-		    if (err) {
-		        return res.render('register', { account : account });
-		    }
-		
-		    passport.authenticate('local')(req, res, function () {
-		      res.redirect('/');
-		    });
+			if (err) {
+				return res.render('register', { account : account });
+			}
+			passport.authenticate('local')(req, res, function () {
+				res.redirect('/');
+			});
 		});
 	});
 	
