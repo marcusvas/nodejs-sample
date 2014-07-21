98bc7e0d8fb23d205e27e52990ff735237ef58c7
Danilo Barsotti <mcbarsotti@gmail.com>
2014-07-21 13:19:30 +0000

    gulp minify css added

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,30 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
+var minifyHtml = require("gulp-minify-html");
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('server/*.js')
+    return gulp.src('./server/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('client/public/javascript/*.js')
+    return gulp.src('./client/public/javascript/*.js')
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('./dist/client/public/js'))
         .pipe(rename('all.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('./dist/client/public/js'));
+});
+
+
+gulp.task('minify-html', function () {
+    gulp.src('./client/public/static_pages/*.html') // path to your files
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('./dist/client/public/static_pages'));
 });
 
 //gulp.task('default', ['lint', 'watch']);
