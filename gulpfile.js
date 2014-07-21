bb2b482989dac33ca66e1417f48a7b5e610627cd
Danilo Barsotti <mcbarsotti@gmail.com>
2014-07-21 12:57:35 +0000

    gulp tasks adde

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('server/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('client/public/javascript/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+//gulp.task('default', ['lint', 'watch']);
+gulp.task('default', ['lint']);
